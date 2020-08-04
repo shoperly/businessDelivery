13048fbee350b6a1f7036d852db9b57a362ee660
LittleRookie1091 <akatsuki1091@gmail.com>
2020-08-04 12:18:14 +1200

    Added domains

--- src/main/java/domain/Delivery.java
+++ src/main/java/domain/Delivery.java
@@ -1,5 +1,14 @@
 package domain;
 
-public class Delivery {
+import lombok.Getter;
+import lombok.Setter;
 
+public class Delivery {
+	@Getter @Setter
+	private String custID;
+	@Getter @Setter
+	private String userName;
+	@Getter @Setter
+	private String password;
+	
 }
